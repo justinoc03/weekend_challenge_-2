39df3c7eadcf15dc905d4e2d44ab272c75d7a44c
Justin O'Connor <justin.oc03@gmail.com>
2016-09-04 14:59:44 -0500

    working version using Dev's example

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,74 +1,86 @@
 console.log('JS is sourced Dawg');
 
+
+/////////////Global Variables////////////////
+var currentIndex = 0;
+var theData;
+var jsonUrl = 'http://devjana.net/pi/pi_students.json';
+
+
 //JQuery is properly sourced
 $(document).ready(function(){
   console.log('JQuery is sourced');
-});
-
-/////////////Global Variables////////////////
-var students = [];
-var jsonUrl = 'http://devjana.net/pi/pi_students.json';
-
-/////////////Display Students on DOM////////////////
-
-var displayStudents = function(){
-  console.log('In displayStudents', students);
-
-  //need a for loop to loop through students and append to the DOM
-  for (var i = 0; i < students.length; i++) {
 
 
-    var newHeader = document.createElement('button');
-    newHeader.textContent = students[i].first_name;
-
-    $('#studentsOutput').append(newHeader);
-  }
-  studentButton();
-  nextStudent();
-};
-
-/////////////clickButton////////////////
-
-var studentButton = function (){
-  console.log('in clickButton');
-  $('#studentsOutput').on('click',function(){
-    console.log('Clicked studentButton!');
+  /////////////AJAX Server Connection////////////////
+  //get data from the server using AJAX
+  $.ajax({
+    url:jsonUrl,
+    dataType: 'JSON',
+    success: function(data){
+      // console.log('in AJAX success: ', data.students);
+      theData = data;
+      displayStudents();
+    } //END AJAX Success
+  }); //END AJAX
 
 
-  });
-};
-
-var nextStudent = function (){
-  console.log('in clickButton');
+  /////////////Previous and Next Buttons////////////////
   $('#nextButton').on('click',function(){
-    console.log('Clicked nextStudent!');
-
-    for (var i = 0; i <students.length; i++) {
-      $('#displayClickedStudent').append(students[i].first_name);
+    currentIndex++;
+    if(currentIndex >= theData.students.length){
+      currentIndex = 0;
     }
-
-  });
-};
-
-
-/////////////AJAX Server Connection////////////////
-
-//get data from the server using AJAX
-$.ajax({
-  url:jsonUrl,
-  dataType: 'JSON',
-  success: function(data){
-    // console.log('in AJAX success: ', data.students);
-
-    //loop through each student and push them into the students array
-    for (var i = 0; i < data.students.length; i++) {
-      students.push(data.students[i]);
-    }
-    //log shows that the global students array is now properly filling
-    // console.log('Global students array: ', students);
-
     displayStudents();
+  }); //end nextButton
 
-  }
+  $('#previousButton').on('click',function(){
+    currentIndex--;
+    if(currentIndex < 0){
+      currentIndex = theData.students.length -1;
+    }
+    displayStudents();
+  }); //end previousButton
+
+  /////////////Display Students on DOM////////////////
+  var displayStudents = function(){
+    $('#studentsOutput').empty();
+
+
+    var nameOut = theData.students[ currentIndex ].first_name + " " + theData.students[ currentIndex ].last_name;
+    // format record number
+
+    var adjustedIndex = currentIndex +1;
+    var counterOut = adjustedIndex + "/" + theData.students.length;
+    /// - create element append - ///
+    // format output
+    var newHeader = document.createElement('h2');
+    newHeader.textContent=nameOut;
+    var newParagraph = document.createElement('p');
+    newParagraph.textContent= counterOut;
+    // display output
+    $("#studentsOutput").append( newHeader );
+    $("#studentsOutput").append( newParagraph );
+  }; // end display student
 
 });
+
+
+
+
+
+// /////////////NextStudent////////////////
+// var nextStudent = function (){
+//   console.log('in clickButton');
+//   $('#nextButton').on('click',function(){
+//     console.log('Clicked nextStudent!');
+//     i++;
+//
+//     if(i >= students.length) {
+//       i = 0;
+//     }
+//       // $('#displayClickedStudent').append(students[i].first_name);
+//
+//
+//   });
+// };
