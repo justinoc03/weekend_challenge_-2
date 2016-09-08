4393fa5b5a6974f4dba870ab15ddc50a3566baa3
Justin O'Connor <justin.oc03@gmail.com>
2016-09-07 22:49:24 -0500

    added timer, still don't quite understand how to call a click even using this or data

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -3,12 +3,33 @@
 /////////////Global Variables///////////////////////////////////////////////////////////////////////////////////
 var students = [];
 var jsonUrl = 'http://devjana.net/pi/pi_students.json';
-var currentPerson = 0;
+var currentPerson = -1;
+var seconds = 0;
 
 
 //JQuery is properly sourced
 $(document).ready(function(){
   // console.log('JQuery is sourced');
+
+/////////////Timer///////////////////////////////////////////////////////////////////////////////////
+var timer = function() {
+  var totalSeconds = 0;
+  var  clock = setInterval(setTime, 1000);
+   function setTime(){
+    ++totalSeconds;
+    var seconds = totalSeconds;
+    console.log(seconds);
+    if(totalSeconds % 10 === 0){
+      // console.log('HOORAY');
+      currentPerson++;
+      displayCurrentStudent();
+    }
+  }
+};
+
+timer();
+console.log(seconds);
+
 
 
 ///////////Display Student INFO on DOM////////////////////////////////////////////////////////////////////////////
@@ -17,13 +38,21 @@ $(document).ready(function(){
 
     var student = students[currentPerson];
 
-    var display = student.first_name + ' ' + student.last_name + '<br>' + student.info;
+    var display = '<strong>' + student.first_name + ' ' + student.last_name + ':</strong>' + '<br>' + student.info;
 
     // console.log(currentPerson + '/' +students.length);
     var counter = currentPerson + 1 + '/' + students.length;
 
-    $('#counter').toggle().fadeIn().html(counter);
-    $('#displayClickedStudent').toggle().fadeIn().html(display);
+    //added fadeIn and fadeOut to display on DOM
+    $('#counter').fadeOut('slow', function(){
+      //stuff that happens after fad eout
+      $('#counter').fadeIn().html(counter);
+    });
+
+    $('#displayClickedStudent').fadeOut('slow', function(){
+      $('#displayClickedStudent').html(display).fadeIn();
+    });
+
   };
 
 
@@ -56,6 +85,7 @@ $(document).ready(function(){
       for (var i = 0; i < students.length; i++) {
         // var newHeader = document.createElement('button');
 
+        //creating buttons for all students that also store data, in this case the data being stored is the 'position', or index, of each student
         var button = $("<button/>");
 
         button.data('index', i );
@@ -73,6 +103,8 @@ $(document).ready(function(){
     var index = $(this).data('index'); //created a key (named index) to recall that value from allStudentButtons function
     // console.log('Student index:', $(this).data('index'));
     // console.log(students[index].first_name);
+
+    //set currentPerson to the index number
     currentPerson = index;
 
     displayCurrentStudent();
@@ -85,7 +117,7 @@ $(document).ready(function(){
     url:jsonUrl,
     dataType: 'JSON',
     success: function(data){
-      // console.log('in AJAX success: ', data.students);
+      console.log('in AJAX success: ', data.students);
 
       //loop through each student and push them into the students array
       for (var i = 0; i < data.students.length; i++) {
@@ -97,6 +129,7 @@ $(document).ready(function(){
       allStudentButtons();
 
     }
+  }); //end AJAX
+//////////////////////Counter////////////////////////////////////////////////////////////////////////////////
 
-  });
-});
+}); //end document ready
